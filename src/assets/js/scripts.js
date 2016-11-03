b17cb3d3ba47b2731df01e1a4c00c6a82022281f
Rafael Meza <rmeza@rcp.pe>
2016-11-03 09:56:17 -0500

    Add loading to index

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
+'use strict';
 
+$(document).ready(function(){
+  loaderHome();
 });
+
+function loaderHome() {
+  var ele = $('.js-hero-image'),
+      originalImage = ele.find('.img');
+
+  $("<img/>")
+    .attr("src", $(originalImage).attr("src"));
+    .on('load', function() {
+        ele.addClass('active');
+    })
+    .on('error', function() {
+      console.log("error loading image");
+    })
+
+}
