13e8c9fe96a697ac31877877d37c8a4ef40f070c
Rafael Meza <rmeza@rcp.pe>
2016-11-03 10:19:15 -0500

    Add loading and buttons animate

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -8,13 +8,17 @@ function loaderHome() {
   var ele = $('.js-hero-image'),
       originalImage = ele.find('.img');
 
+  var btn_1 = $('.js-button-1'),
+      btn_2 = $('.js-button-2');
+
   $("<img/>")
-    .attr("src", $(originalImage).attr("src"));
+    .attr("src", $(originalImage).attr("src"))
     .on('load', function() {
         ele.addClass('active');
+        setTimeout(function(){ btn_1.addClass('active'); }, 800);
+        setTimeout(function(){ btn_2.addClass('active'); }, 1600);
     })
     .on('error', function() {
       console.log("error loading image");
-    })
-
+    });
 }
